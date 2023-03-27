079a3a091512d54244c0b49d1d7ca108f01a3fea
Moisema <moisemadukuly82@gmail.com>
2023-03-27 08:33:37 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+ * main -function that takes a pointer to an as parameter and updates the value
+ *
+ * Return: Aways 0.
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+}

c90033a0d40d7470e90793e610ffdfd52db7b390
Moisema <moisemadukuly82@gmail.com>
2023-03-29 19:44:22 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * 101-print_number - Print an integer
+ * @n: Integer
+ *Return: Always 0 (success)
+ */
+
+void print_number(int n)
+{
+	int n = 10;
+
+	_putchar("%d\n", n);
+
+	return (0);
+
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,4 +6,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-
+char *rot13(char *);
+void print_number(int n);
+char *infinte_add(char *n1, *n2, char *r, int size_r);
+void print_buffer(char *b, int size);

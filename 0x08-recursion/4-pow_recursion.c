13bce4211efad04f34cfc17f4dd4c7ad07384d2c
Moisema <moisemadukuly82@gmail.com>
2023-04-05 14:58:30 -0700

    0-puts_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,1 +1,22 @@
 #include "main.h"
+
+/**
+ * int _pow_recursion(int x, int y) - Return the of x
+ * @x: value of x
+ * @y: Power of y
+ * Reurn: Always 1 (success)
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
+

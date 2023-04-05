7497bd854aaaa0228c26cfa91d2533c49ebfd49d
Moisema <moisemadukuly82@gmail.com>
2023-04-05 14:42:07 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,15 +7,11 @@
  * Return: String
  */
 
-void _puts_recursion(char *s)
-{
-	if (*s != '\0')
-	{
-		putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-	{
-		putchar('\n');
-	}
+void _print_rev_recursion(char *s) {
+    if (*s == '\0') {
+        return;  
+        
+    }
+    _print_rev_recursion(s + 1);  
+     putchar(*s);  
 }

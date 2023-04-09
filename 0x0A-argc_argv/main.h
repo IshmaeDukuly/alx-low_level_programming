ba5590b909544915f39adb99bf2cf7ee650cf749
Moisema <moisemadukuly82@gmail.com>
2023-04-09 05:46:30 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,7 +3,10 @@
 
 
 
-
+int main(int argc, char *argv[])
+int main(int argc, char argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 
 
 #endif

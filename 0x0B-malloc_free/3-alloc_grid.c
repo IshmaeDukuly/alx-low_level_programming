d7385061d0f269d0754cb25b20ddf80452db0ed4
Moisema <moisemadukuly82@gmail.com>
2023-04-11 05:33:13 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,7 +3,6 @@
 
 /**
  * 3-alloc_grid.c - A function that returns to a dimensional array
- * of integers
  * @width: The integer
  * @height: Height of an integer
  * Return: 0 Success
@@ -25,22 +24,23 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 	for (a = 0; a < height; a++)
-	{
+
 		 grid[a] = (int *) malloc(width * sizeof(int));
 
 		if (grid[a] == NULL)
-		{
+
 			for (b = 0; b < a; b++)
-			{
+
 				free(grid[b]);
-			}
+
+
 			free(grid);
 			return (NULL);
-		}
-		for (b = 0; b < width; b++)
-		{
+
+	for (b = 0; b < width; b++)
+
 			 grid[a][b] = 0;
-		}
-	}
+
+
 	return (grid);
 }

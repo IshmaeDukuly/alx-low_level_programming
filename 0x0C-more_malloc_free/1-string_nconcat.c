f13a204611b02a5007e6a22557434e8e1800eea1
Moisema <moisemadukuly82@gmail.com>
2023-04-13 05:45:11 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,31 +2,26 @@
 
 /**
  * string_nconcat - the returned shall point to a newly allocated
- * space 
+ * space
  * @s1: firts string
  * @s2: second string
- * @n: Bytes 
+ * @n: Bytes
  * Return: string follow the bytes
  */
-
-
-
-
-
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int l1, l2, x, y;
 	char *str;
 
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
 	l1 = l2 = 0;
-	
+
 	while (s1[l1] != '\0')
 		l1++;
 	while (s2[l2] != '\0')
@@ -39,10 +34,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (str == NULL)
 		return (NULL);
 
-	
+
 	for (x = 0; s1[x] != '\0'; x++)
 		str[x] = s1[x];
-	
+
 	for (y = 0; y < n && s2[y] != '\0'; y++)
 	{
 		str[x] = s2[y];

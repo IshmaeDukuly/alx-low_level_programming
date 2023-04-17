d07f2e23ac4d7bdbe10755f9d733cc4f27ce99e6
Moisema <moisemadukuly82@gmail.com>
2023-04-17 05:58:15 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+#This is alx-low-level_c Programming '0x0E-structure_typedef Project'
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG_h_
+#define DOG_h_
+
+/**
+ * struct dog - this is the type
+ * @name: string datape type
+ * @age: int datatype
+ * @owner: string datatype
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d)
+
+
+
+#endif 

31378d5fd7fc95b3d85f5db6b44eec4959a60fac
Moisema <moisemadukuly30@gmail.com>
2023-04-20 04:08:07 -0700

    README.md

---
+++ 0x10-variadic_functions/1-main.c
@@ -0,0 +1,14 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_numbers(", ", 4, 0, 98, -1024, 402);
+    return (0);
+}
+julien@ubuntu:~/0x0f. variadic functions$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-print_numbers.c -o b
+
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+#This is alx-low_level_programming '0x10-variadic_functions' project

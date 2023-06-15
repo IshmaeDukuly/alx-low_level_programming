7c8f3b87ff558a8c16743ef04519dc8abd5445a6
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-06-15 04:43:47 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * print_dlistint - print all the items
  * of a doubl linked list
@@ -8,13 +7,11 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-
 	const dlistint_t *temp;
 	size_t m = 0;
 	temp = h;
 	while (temp != NULL)
 	{
-
 		printf("%d\n", temp->n);
 		temp = temp->next;
 		m++;

584b8ff6603702ec6d0387c03ebb5da9d6f2ad52
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-06-16 09:24:52 -0700

    103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,73 +1,19 @@
-#include "lists.h"
-#include <string.h>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #include <stdio.h>
-#inclue <string.h>
-#include <stdlb.h>
+#include <string.h>
+#include <stdlib.h>
 
 
 /**
  * main - this generate key base on username
  * @argc: the arguments passed
- * @rgv: arguments passed
+ * @argv: arguments passed
  * Return: Always return 0 success
  */
-int main(int argc, char *rgv[])
+int main(int argc, char *argv[])
 {
 	unsigned int y, b;
-	size_t len , add;
-	char *1 = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	size_t len, add;
+	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	char p[7] = "  ";
 
 	if (argc != 2)
@@ -77,7 +23,7 @@ int main(int argc, char *rgv[])
 	}
 	len = strlen(argv[1]);
 	p[0] = l[(len ^ 59) & 63];
-	for (y = 0, add = 0; i < len; y++)
+	for (y = 0, add = 0; y < len; y++)
 		add += argv[1][y];
 	p[1] = l[(add ^ 79) & 63];
 	for (y = 0, b = 1; y < len; y++)
@@ -90,11 +36,11 @@ int main(int argc, char *rgv[])
 	srand(b  ^ 14);
 	p[3] = l[rand() & 63];
 	for (b  = 0, y = 0; y < len; y++)
-		b += argv[1][y] *argv[1][y];
+		b += argv[1][y] * argv[1][y];
 	p[4] = l[(b ^ 239) & 63];
 	for (b = 0, y = 0; (char)y < argv[1][0]; y++)
 		b = rand();
-	p[5] = l[(b ^ 239) & 63 ];
+	p[5] = l[(b ^ 239) & 63];
 	printf("%s\n", p);
 	return (0);
 }

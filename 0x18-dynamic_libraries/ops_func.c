143e6e013a8224e02a1971c01e4769ce87674639
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-06-19 15:01:59 -0700

    1-create_dynamic_lib.sh

---
+++ 0x18-dynamic_libraries/ops_func.c
@@ -0,0 +1,54 @@
+/**
+ * add -addtion
+ * @a: integer
+ * @b: 2nd integer
+ * Return: something
+ */
+int add(int a, int b)
+{
+
+	return (a + b);
+}
+
+/**
+ * sub - substraction
+ * @a: integer
+ * @b: 2nd interger
+ * Return: Somethin
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplication
+ * @: 1st integer
+ * @b: 2nd integer
+ * Return: something
+ */
+int mul(int a, int b)
+{
+	return (a * b)
+}
+/**
+ * div - division
+ * @: 1st integer
+ * @b: 2nd integer
+ * Return: something
+ */
+int div(int a, int b)
+{
+	return (a / b)
+}
+/**
+ * mod - modulus for remainder
+ * @: 1st integer
+ * @b: 2nd integer
+ * Return: something
+ */
+int mod(int a, int b)
+{
+
+	return (a % b);
+}

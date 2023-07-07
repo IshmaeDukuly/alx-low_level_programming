8cbe411a388635dc27fc2c570345df97269dd967
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-07-07 06:20:29 -0700

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -42,7 +42,7 @@ int create_and_add_node(hash_table_t *ht, const char *key, const char *value,
 
 
 	if ((ht->array)[idx] == NULL)
-		node1-> = NULL;
+		node1->next = NULL;
 	else
 		node1->next = (ht->array)[idx];
 	(ht->array)[idx] = node1;
@@ -55,3 +55,34 @@ int create_and_add_node(hash_table_t *ht, const char *key, const char *value,
  * @ht: the hash table
  * @key: not empty
  * @value: the value
+ * Return: Always 1
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+	unsigned long int idx;
+	hash_node_t *node1 = NULL;
+	char *n;
+
+	if (!ht || !(ht->array) || !key || strlen(key) == 0 || !value)
+		return (0);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+
+	node1 = (ht->array)[idx];
+	while (node1 && (strcmp(key, node1->key) != 0))
+		node1 = node1->next;
+	if (node1 != NULL)
+	{
+		n = strdup(value);
+		if (!n)
+			return (0);
+		if (node1->value)
+			free(node1->value);
+		node1->value = n;
+		return (1);
+	}
+
+	return (create_and_add_node(ht, key, value, idx));
+
+}

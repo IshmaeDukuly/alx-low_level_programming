c04df5b2b517ff75814dde10d8cfbdaeee471c4f
IshmaeDukuly <moisemadukuly82@gmail.com>
2023-07-07 07:00:36 -0700

    1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,8 @@
+/**
+ * hash_djb2 - the djb2 algorithm
+ * @str: the hash value
+ * Retun: the # value
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
     unsigned long int hash;
@@ -6,7 +11,7 @@ unsigned long int hash_djb2(const unsigned char *str)
     hash = 5381;
     while ((c = *str++))
     {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
     }
     return (hash);
 }
